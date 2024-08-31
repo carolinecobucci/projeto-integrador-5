583335170032519d0fd0d933da06b42e679c3a68
Caroline Cobucci <cacobuti@gmail.com>
2024-08-31 18:11:29 -0300

    adds: select and skeleton

---
+++ src/components/common/Select/Select.tsx
@@ -0,0 +1,31 @@
+import { forwardRef, Ref, SelectHTMLAttributes } from "react";
+import styles from "./Select.module.css";
+
+interface ISelect extends SelectHTMLAttributes<HTMLSelectElement> {
+  label: string;
+  options: {
+    value: string;
+    text: string;
+  }[];
+}
+
+function FowardedSelect(
+  { label, options, ...rest }: ISelect,
+  ref: Ref<HTMLSelectElement>
+) {
+  return (
+    <div className={styles.selectGroup}>
+      <label>{label}</label>
+      <select className={styles.selectBase} ref={ref} {...rest}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+const Select = forwardRef(FowardedSelect);
+export { Select };
